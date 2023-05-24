95e24689de160069b4d6b8609520d686cc68cf5d
Vikas Singh <Vikas@olibr.com>
2023-05-24 15:17:41 +0530

    added download floating and emebbeding google forms

--- js_files/main.js
+++ js_files/main.js
@@ -265,3 +265,17 @@ submitButton.addEventListener("click", (e) => {
 //     enquiry: enquiry
 //   });
 // }
+
+
+
+    const modal = document.querySelector("#modal");
+    const openModal = document.querySelector(".open-button");
+    const closeModal = document.querySelector(".close-button");
+
+    openModal.addEventListener("click", () => {
+      modal.showModal();
+    });
+
+    closeModal.addEventListener("click", () => {
+      modal.close();
+    });
